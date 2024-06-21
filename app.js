bffb73ad554bbfa3956e430f984943af62c03bfb
syedimad77 <imad2000syed@gmail.com>
2024-06-22 01:24:51 +0530

    first commit

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
+const cluster = require('node:cluster');
+const http = require('node:http');
+const os = require('node:os');
+const process = require('node:process');
 const express = require("express");
-const http = require("http");
 const { Server } = require("socket.io");
 const cors = require('cors');
 require('dotenv').config();
 
-const app = express();
-const port = process.env.PORT || 3002;
-const server = http.createServer(app);
+const numCPUs = os.availableParallelism();
 
-// Configure CORS for Express
-app.use(cors());
+if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
 
-// Socket.IO configuration
-const io = new Server(server, {
-    cors: {
-        origin: "*", // Replace with your frontend's URL
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+        // Optionally fork a new worker when one dies
+        cluster.fork();
+    });
+} else {
+    const app = express();
+    const port = process.env.PORT || 3002;
+    const server = http.createServer(app);
+
+    // Configure CORS for Express
+    app.use(cors({
+        origin: "*", // Replace "*" with your frontend's URL in production
         methods: ["GET", "POST"],
         allowedHeaders: ["Content-Type"],
         credentials: true
-    }
-});
+    }));
 
-// Import and setup routes
-const setupRoutes = require('./routes');
-setupRoutes(app, io);
+    // Socket.IO configuration
+    const io = new Server(server, {
+        cors: {
+            origin: "*", // Replace "*" with your frontend's URL in production
+            methods: ["GET", "POST"],
+            allowedHeaders: ["Content-Type"],
+            credentials: true
+        }
+    });
 
-server.listen(port, () => {
-    console.log(`Server listening on the port::${port}`);
-});
+    // Middleware to parse JSON bodies
+    app.use(express.json());
+
+    // Import and setup routes
+    const setupRoutes = require('./routes');
+    setupRoutes(app, io);
+
+    server.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
+
+    console.log(`Worker ${process.pid} started`);
+}
